846cf5715cfa5ed13715f965373c0ff1cb3103f3
Koshkarik <koshkarevj@mail.ru>
2018-02-28 11:48:14 +0300

    change rss logic

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import axios from 'axios';
 import validator from 'validator';
-import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const inputField = document.querySelector('input');
 const form = document.querySelector('form');
 const streamsUl = document.querySelector('.streams-list');
 const itemsWrapper = document.querySelector('.all-items');
+const proxyCorsServer = 'https://api.codetabs.com/cors-proxy/';
 
-const listOfStreams = [];
+const state = {
+  inputForm: {
+    valid: false,
+    activeClass: '',
+  },
+  activeStreams: ['http://feeds.bbci.co.uk/news/world/africa/rss.xml'],
+  listOfStreams: [],
+};
 
 const checkUrl = url => validator.isURL(url);
 
@@ -57,51 +64,64 @@ const getAllItemsNodes = (streamsList) => {
   });
 };
 
-const handleInputClass = (inputEl) => {
-  const { classList } = inputEl;
-  const valid = checkUrl(inputEl.value);
-  const classToAdd = valid ? 'is-valid' : 'is-invalid';
-  const classToRemove = valid ? 'is-invalid' : 'is-valid';
-  if (inputEl.value.length === 0) {
-    classList.remove('is-invalid');
-    classList.remove('is-valid');
-    return;
-  }
-  if (classList.contains(classToRemove)) {
-    classList.remove(classToRemove);
-  }
-  if (!classList.contains(classToAdd)) {
-    classList.add(classToAdd);
-  }
+const getStream = (url) => {
+  const requestUrl = proxyCorsServer.concat(url);
+  axios.get(requestUrl)
+    .then((response) => {
+      const parser = new DOMParser();
+      const parsedXml = parser.parseFromString(response.data, 'application/xml');
+      const newChannelData = getInfoFromXml(parsedXml);
+      const { listOfStreams } = state;
+      listOfStreams.push(newChannelData);
+      const allStreamsNodes = getAllStreamsListNodes(listOfStreams);
+      streamsUl.innerHTML = '';
+      allStreamsNodes.forEach(node => streamsUl.append(node));
+      const allItems = getAllItemsNodes(listOfStreams);
+      allItems.forEach((item) => {
+        itemsWrapper.append(item);
+      });
+      if (!state.activeStreams.includes(url)) {
+        state.activeStreams.push(url);
+      }
+    });
 };
 
-$(() => {
+const handleInputClass = (inputEl) => {
+  const { classList } = inputEl;
+  const { activeClass } = state.inputForm;
+  const valid = checkUrl(inputEl.value);
+  state.inputForm.valid = valid;
+  const classToAdd = state.inputForm.valid ? 'is-valid' : 'is-invalid';
+  const classToRemove = state.inputForm.valid ? 'is-invalid' : 'is-valid';
+  if (inputEl.value.length === 0) {
+    classList.remove(activeClass);
+    state.inputForm.activeClass = '';
+    return;
+  }
+  if (activeClass === classToRemove) {
+    classList.remove(classToRemove);
+  }
+  classList.add(classToAdd);
+  state.inputForm.activeClass = classToAdd;
+};
+
+const runRss = () => {
+  const promises = state.activeStreams.map(stream => getStream(stream));
+  axios.all(promises);
   inputField.addEventListener('input', (e) => {
     handleInputClass(e.target);
   });
   form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const url = inputField.value.includes('http') ? inputField.value : 'http://'.concat(inputField.value);
-    if (!checkUrl(url)) {
+    const url = inputField.value;
+    if (!state.inputForm.valid) {
       return;
     }
     inputField.value = '';
     handleInputClass(inputField);
-    const targetUrl = new URL('/feed', url);
-    console.log(targetUrl);
-    axios.get(targetUrl)
-      .then((response) => {
-        const parser = new DOMParser();
-        const parsedXml = parser.parseFromString(response.data, 'application/xml');
-        const newChannelData = getInfoFromXml(parsedXml);
-        listOfStreams.push(newChannelData);
-        const allStreamsNodes = getAllStreamsListNodes(listOfStreams);
-        streamsUl.innerHTML = '';
-        allStreamsNodes.forEach(node => streamsUl.append(node));
-        const allItems = getAllItemsNodes(listOfStreams);
-        allItems.forEach((item) => {
-          itemsWrapper.append(item);
-        });
-      });
+    getStream(url);
   });
-});
+};
+
+runRss();
+
